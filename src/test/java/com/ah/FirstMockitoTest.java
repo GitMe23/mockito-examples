35bec424293e8f9645275678eea95c230a6e39e5
Andy Hopwood <andyhopwood@andys-mbp.home>
2022-07-21 15:43:06 +0100

    Added System Under Test (SUT)

---
+++ src/test/java/com/ah/FirstMockitoTest.java
@@ -0,0 +1,22 @@
+package com.ah;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * TodoService.java => Wunderlist
+ */
+
+
+
+
+public class FirstMockitoTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
